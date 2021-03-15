7ed29b3961a40a30630c9de4b06033d23721e6ae
Scott Block <scottrblock@gmail.com>
2021-03-14 21:48:50 -0400

    blocking 1

--- src/entries/phone-to-roam.ts
+++ src/entries/phone-to-roam.ts
@@ -1,4 +1,4 @@
-import { toRoamDate, toRoamDateUid, genericError, pushBullets } from 'roam-client'
+import { toRoamDate, toRoamDateUid, genericError, pushBullets, WindowClient } from 'roam-client'
 import axios from "axios";
 import { formatRFC3339, startOfDay, endOfDay } from "date-fns";
 
@@ -8,17 +8,22 @@ axios(`https://www.phonetoroam.com/messages.json?roam_key=${roamKey}`).then(asyn
   console.log('mylog 1', res)
   console.log('mylog 2', res.data)
   res.data.forEach((item) => {
-    try {
-      const date = item['created_at']
-      const title = toRoamDate(date)
-      const parentUid = toRoamDateUid(date)
+    const date = item['created_at']
+    const title = toRoamDate(date)
+    const parentUid = toRoamDateUid(date)
+    console.log('mylog 3', parentUid)
+    // const  queryResults = () => { async(
+    //     await 
+    //   )
 
-      pushBullets([item['body']], parentUid, parentUid)
-    } catch(e) {
-      console.error(e.response.data.error)
-    }
-    
+    // } 
+      
 
-    console.log('mylog 3', item)
+    // console.log('mylog 3', item)
+    // const parentUid = toRoamDateUid(item['created_at'])
+    // client.appendBlock({
+    //   text: item['body'],
+    //   parentUid: parentUid
+    // })
   })
 }).catch((e) => genericError(e))
