76eef65913da461fc80cc245e5e1c2a79a39ad85
Scott Block <scottrblock@gmail.com>
2021-03-14 20:00:21 -0400

    update message endpoint

--- src/entries/phone-to-roam.ts
+++ src/entries/phone-to-roam.ts
@@ -4,7 +4,7 @@ import { formatRFC3339, startOfDay, endOfDay } from "date-fns";
 
 const roamKey = document.getElementById('phone-to-roam-script')?.dataset.roam_key
 
-axios(`https://phonetoroam.com/messages?roamKey=${roamKey}`).then(async (res: any) => {
+axios(`https://www.phonetoroam.com/messages.json?roam_key=${roamKey}`).then(async (res: any) => {
   console.log('res', res)
 }).catch((e: any) => genericError(e))
 
