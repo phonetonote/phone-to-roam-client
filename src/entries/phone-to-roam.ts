cf030e8e1dcb51be1fb54aabf4721c33f8cb8291
Scott Block <scottrblock@gmail.com>
2021-03-14 22:29:32 -0400

    close, need to make daily page

--- src/entries/phone-to-roam.ts
+++ src/entries/phone-to-roam.ts
@@ -3,18 +3,45 @@ import axios from "axios";
 import { formatRFC3339, startOfDay, endOfDay } from "date-fns";
 
 const roamKey = document.getElementById('phone-to-roam-script')?.dataset.roam_key
+const findPage: any = async (pageName, uid) => { 
+   const queryResults = await window.roamAlphaAPI.q(
+      `[:find (pull ?e [:block/uid]) :where [?e :node/title "${pageName}"]]`
+    );
+    
+    if (queryResults.length === 0) {
+      const basicPage: any = await window.roamAlphaAPI.createPage({
+        page: {
+          title: pageName,
+          uid: uid
+        }
+      });
+      return basicPage.uid;
+    }
+    return queryResults[0]["block/uid"];
+}
 
 axios(`https://www.phonetoroam.com/messages.json?roam_key=${roamKey}`).then(async (res) => {
   console.log('mylog 1', res)
   console.log('mylog 2', res.data)
   res.data.forEach((item) => {
     const date = new Date(item['created_at'])
-    console.log('mylog date', date)
     const title = toRoamDate(date)
-    console.log('mylog title', title)
-
     const parentUid = toRoamDateUid(date)
+
+
+    const newParentUid = findPage(title, parentUid)
+    
     console.log('mylog parentUid', parentUid)
+    console.log('mylog newParentUid', newParentUid)
+    // window.roamAlphaAPI.createBlock({
+    //   location: {
+    //     "parent-uid": parentUid,
+    //     order: 999999
+    //   },
+    //   block: {
+    //     string: item['body'],
+    //   }
+    // })
     // const  queryResults = () => { async(
     //     await 
     //   )
