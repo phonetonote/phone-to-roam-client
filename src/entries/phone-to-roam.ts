64b031edb289f85ec36a28a38d1706d680326ba9
Scott Block <scottrblock@gmail.com>
2021-04-04 19:53:43 -0400

    fix order

--- src/entries/phone-to-roam.ts
+++ src/entries/phone-to-roam.ts
@@ -12,15 +12,21 @@ if(roamKey) { Bugsnag.setUser(roamKey, undefined, undefined) }
 
 const fetchNotes = () => {
   axios(`${SERVER_URL}/messages.json?roam_key=${roamKey}`).then(async (res) => {
-    res.data.forEach(async (message) => {
+    let order = 0
+    res.data.forEach(async (message, i) => {
       const node = nodeMaker(message)
       const date = new Date(message['created_at'])
       const title = toRoamDate(date)
       const oldParentId = toRoamDateUid(date)
       const parentUid = await findPage(title, oldParentId)
       const childrenQuery = window.roamAlphaAPI.q(`[ :find (pull ?e [* {:block/children [*]}]) :where [?e :node/title "${title}"]]`)
-      console.log('childrenQuery', childrenQuery)
-      const order = childrenQuery ? (childrenQuery[0][0]?.children?.length || 0) : 0
+      console.log('ptr logging', [i, childrenQuery, order])
+      if(i === 0) {
+        order = childrenQuery ? (childrenQuery[0][0]?.children?.length || 0) : 0
+      } else {
+        order = order + 1
+      }
+      
       
       createBlock({ node, parentUid, order })
       axios.patch(`${SERVER_URL}/messages/${message.id}.json?roam_key=${roamKey}`, {"status": "published"})
