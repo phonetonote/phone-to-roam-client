d4500cf591e693e3d7f1dc23e53fdd3348acaff8
Scott Block <scottrblock@gmail.com>
2021-04-28 13:38:22 -0400

    parent block and hashtag config

--- src/entries/phone-to-roam.ts
+++ src/entries/phone-to-roam.ts
@@ -3,6 +3,10 @@ import axios from "axios";
 import { formatRFC3339, startOfDay, endOfDay } from "date-fns";
 import { findPage, createBlock, nodeMaker } from "../entry-helpers";
 import Bugsnag from '@bugsnag/js'
+import { createConfigObserver } from "roamjs-components";
+
+const ID = "ptr";
+const CONFIG = `roam/js/${ID}`;
 
 const SERVER_URL = 'https://www.phonetoroam.com'
 const roamKey = document.getElementById('phone-to-roam-script')?.dataset.roam_key
@@ -37,7 +41,35 @@ const fetchNotes = () => {
   })
 }
 
+const configure = () => {
+  createConfigObserver({
+    title: CONFIG,
+    config: {
+      tabs: [
+        {
+          id: "home",
+          fields: [
+            {
+              type: "text",
+              title: "hashtag",
+              description: "if you want  #hashtag at the end of each phonetoroam note, put what you want that hashtag to be here. if you do not want a hashtag, make this blank.",
+              defaultValue: "#phonetoroam"
+            },            
+            {
+              type: "text",
+              title: "parent block title",
+              description: "if you want your phonetoroam notes nested under a block, give that block a name here. if you do not want them nested under anything, leave this blank.",
+              defaultValue: "phonetoroam notes"
+            },
+          ],
+        },
+      ],
+    },
+  });
+}
+
 fetchNotes()
+configure()
 
 document.addEventListener('click', (e: any) =>{
   if(e.target.innerText === 'DAILY NOTES') {
