6bdb21f85cebf12f57d41291bae3eb93e5b8880e
Scott Block <scottrblock@gmail.com>
2021-04-30 19:35:11 -0400

    cleanup

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+// #TODO this should change based off some netlify env variable
+export const SERVER_URL = 'https://phonetoroam.com'
+
+export const ID = "ptr";
+export const CONFIG = `roam/js/${ID}`;
+export const DEFAULT_HASHTAG = "phonetoroam"
+export const LINK_KEYS = ['title', 'description', 'site_name', 'content_type']
