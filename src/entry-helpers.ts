b813463ff3b007b692b013a1d5106d2f0612de10
Scott Block <scottrblock@gmail.com>
2021-05-03 17:37:58 -0400

    ?

--- src/entry-helpers.ts
+++ src/entry-helpers.ts
@@ -141,7 +141,7 @@ export const findParentUid: any = async (pageName, uid) => {
 
   if(parentBlock && typeof(parentBlock) === 'string' && parentBlock.length > 0) {
     const children = await window.roamAlphaAPI.q(
-      `[:find (pull ?e [* {:block/children [*]}]) :where [?e :node/title "${pageName}"]]`)[0][0]['children']
+      `[:find (pull ?e [* {:block/children [*]}]) :where [?e :node/title "${pageName}"]]`)[0][0]['children'] || []
     const potentialParentBlock = children.filter((item) => item['string'] === parentBlock)
     if(potentialParentBlock.length > 0) {
       return potentialParentBlock[0]['uid']
