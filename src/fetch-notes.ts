4f1c2b5848792054c60fbcfcf2f7b93630a069eb
Scott Block <scottrblock@gmail.com>
2021-05-18 18:00:11 -0400

    remove all logs

--- src/fetch-notes.ts
+++ src/fetch-notes.ts
@@ -13,13 +13,9 @@ export const roamKey = document.getElementById(SCRIPT_ID)?.dataset.roam_key
 export const fetchNotes = async (hashtag) => {
   axios(`${SERVER_URL}/messages.json?roam_key=${roamKey}`).then(async (res) => {
     const messagesByPageName = res.data.reduce(reduceMessages, {})
-    console.log('\n\nptr log *******************************')
-    console.log('ptr log messagesByPageName', messagesByPageName)
 
     for(const pageName in messagesByPageName) {
-      console.log('ptr log pageName', pageName)
       const messages = messagesByPageName[pageName];
-      console.log('ptr log messages', messages)
       const date = new Date(messages[0]['created_at']), 
             parentUid = findOrCreateParentUid(date, parentBlock, window.roamAlphaAPI, createBlock), // todo replace roam API with roamjs typed version,
             localStartingOrder = startingOrder(parentUid, window.roamAlphaAPI)
@@ -30,9 +26,6 @@ export const fetchNotes = async (hashtag) => {
         await axios.patch(`${SERVER_URL}/messages/${message.id}.json?roam_key=${roamKey}`, {"status": "published"})
       }
     }
-
-    console.log('ptr log *******************************\n\n')
-
   }).catch((e) => {
     console.log('phonetoroam error', e)
     Bugsnag.notify(e)
