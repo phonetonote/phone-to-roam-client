0c96bd194db66c6e38dc21e874e952b21ccb4dc1
Scott Block <scottrblock@gmail.com>
2021-08-04 15:04:07 -0600

    dont add extra tags

--- src/node-maker.ts
+++ src/node-maker.ts
@@ -1,48 +1,53 @@
-import { TextNode } from 'roam-client'
-import { LINK_KEYS } from './constants'
+import { TextNode } from "roam-client";
+import { LINK_KEYS } from "./constants";
 
 export const nodeMaker = (message, hashtag) => {
-  const children: TextNode[] = []
-  const attachment = message?.attachments[0]
-  let text = message['text']
+  const children: TextNode[] = [];
+  const attachment = message?.attachments[0];
+  let text = message["text"];
 
-  if(attachment?.media_type === 'link') {
-    if(attachment.image_url && attachment.image_url.length > 0) {
+  if (attachment?.media_type === "link") {
+    if (attachment.image_url && attachment.image_url.length > 0) {
       children.push({
-        text: `![](${attachment['image_url']})`,
-        children: []
-      })
+        text: `![](${attachment["image_url"]})`,
+        children: [],
+      });
     }
 
     LINK_KEYS.forEach((k) => {
-      const v = attachment[k]
-      if(v?.length > 0) {
+      const v = attachment[k];
+      if (v?.length > 0) {
         children.push({
-          text: `${k.replace(/_/g, ' ')}:: ${v.trim()}`,
-          children: []
-        })
+          text: `${k.replace(/_/g, " ")}:: ${v.trim()}`,
+          children: [],
+        });
       }
-    })
-
-  } else if (attachment?.media_type === 'image') {
-    text = `![](${attachment.url})`
-  } else if (attachment?.media_type === 'audio') {
-    const title = message.body?.trim()?.length > 0 ? message.body : 'Audio Recording' 
-    text = `[${title}](${attachment.url})`
+    });
+  } else if (attachment?.media_type === "image") {
+    text = `![](${attachment.url})`;
+  } else if (attachment?.media_type === "audio") {
+    const title =
+      message.body?.trim()?.length > 0 ? message.body : "Audio Recording";
+    text = `[${title}](${attachment.url})`;
   }
 
-  text = `${text.trim()}`
-  if(hashtag && (typeof(hashtag) === 'string') && (hashtag.length > 0)) {
-    text = `${text} #${hashtag}`
+  text = `${text.trim()}`;
+  const validHashtag =
+    hashtag && typeof hashtag === "string" && hashtag.length > 0;
+  const existingTags = /#\w+toroam/;
+  const needsNewTag = !!!text.match(existingTags).length;
+
+  if (validHashtag && needsNewTag) {
+    text = `${text} #${hashtag}`;
   }
 
-  if(message?.sender_type === 'facebook') {
-    text += ' #facebooktoroam'
-  } else if (message?.sender_type === 'alfred') {
-    text += ' #alfredtoroam'
-  } else if (message?.sender_type === 'telegram') {
-    text += ' #telegramtoroam'
+  if (message?.sender_type === "facebook") {
+    text += " #facebooktoroam";
+  } else if (message?.sender_type === "alfred") {
+    text += " #alfredtoroam";
+  } else if (message?.sender_type === "telegram") {
+    text += " #telegramtoroam";
   }
 
-  return { text: `${text}`, children: children }
-}
+  return { text: `${text}`, children: children };
+};
