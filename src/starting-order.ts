e7ab6a22dc752f064600372e3412f5d56c16ef6e
Scott Block <scottrblock@gmail.com>
2022-07-26 23:47:56 -0400

    remove dependencies

--- src/starting-order.ts
+++
@@ -1,9 +0,0 @@
-export const startingOrder = (
-  parentUid: string,
-  roamAPI: Window["roamAlphaAPI"]
-): number => {
-  const childrenQuery = roamAPI.q(
-    `[ :find (pull ?e [* {:block/children [*]}]) :where [?e :block/uid "${parentUid}"]]`
-  );
-  return childrenQuery?.[0]?.[0]?.children?.length || 0;
-};
