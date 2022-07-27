e1a81448379d3fec66301bc1955b80f959435beb
Scott Block <scottrblock@gmail.com>
2022-07-26 23:46:17 -0400

    roam depot message

--- src/entries/phone-to-roam.ts
+++ src/entries/phone-to-roam.ts
@@ -1,19 +1,5 @@
-import { fetchNotes, roamKey } from "../fetch-notes";
-import { configure, configValues } from "../configure";
-import Bugsnag from "@bugsnag/js";
+alert(
+  "please install phonetonote from roam depot and remove the {{roam/js}} script. thanks for the early support!"
+);
 
-Bugsnag.start({ apiKey: "0ca67498b27bd9e3fba038f7fb0cd0b4" });
-if (roamKey) {
-  Bugsnag.setUser(roamKey, undefined, undefined);
-}
-
-configure();
-fetchNotes();
-
-document.addEventListener("click", (e: any) => {
-  if (e?.target?.innerText?.toUpperCase() === "DAILY NOTES") {
-    fetchNotes();
-  }
-});
-
-window.setInterval(() => fetchNotes(), 1000 * 60);
+export {};
